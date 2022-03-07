e35bae2820d1e1336392a82a563e9d74b0fc21f0
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2022-03-07 15:56:19 -0500

    deploying issues

--- public/js/idb.js
+++ public/js/idb.js
@@ -57,7 +57,7 @@ function saveRecord(record) {
 getAll.onsuccess = function() {
     // if there was data in indexedDb's store, let's send it to the api server
     if (getAll.result.length > 1) {
-      fetch('./api/transaction/bulk', {
+      fetch('/api/transaction/bulk', {
         method: 'POST',
         body: JSON.stringify(getAll.result),
         headers: {
@@ -83,7 +83,7 @@ getAll.onsuccess = function() {
           console.log(err);
         });
     } else if (getAll.result.length > 0) {
-        fetch('./api/transaction', {
+        fetch('/api/transaction', {
           method: 'POST',
           body: JSON.stringify(getAll.result),
           headers: {
